89b6fbe4fec703d44d4b1a10a8423abd231e3f13
Admin3 <Admin3@demo-1>
2020-06-16 21:40:12 +0000

    Add project files.

---
+++ Libraries/Nop.Services/Orders/CustomeCheckOut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage.Blob.Protocol;
+
+namespace Nop.Services.Orders
+{
+    public class CustomeCheckOut
+    {
+        public int CustomerId { get; set; }
+        public ICollection<CutomeProduct> CustomeProducts { get; set; }
+        public CustomeCheckOut()
+        {
+            CustomeProducts = new HashSet<CutomeProduct>();
+        }
+
+    }
+    public class CutomeProduct
+    {
+        public string SKU { get; set; }
+        public decimal Price { get; set; }
+        public int Qunatity { get; set; }
+    }
+}
